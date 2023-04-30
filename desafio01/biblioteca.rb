47e47e52a5d12a610e2c9ba0169e2be8386b9202
Éver <83778270+evertondtf@users.noreply.github.com>
2023-04-30 19:53:33 -0300

    atualização

---
+++ desafio01/biblioteca.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "O leitor #{nome} #{sobrenome}, de #{idade} anos, acessou a biblioteca."
